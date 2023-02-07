2d495eb3fc91605fa59290e12a2516e746eecaf6
Alexandre Júnior <alexandrepj13@gmail.com>
2023-02-06 21:17:11 -0300

    Project finaly

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+const btnLigar = document.querySelector(".btn_ligar")
+btnLigar.addEventListener("click",()=>{
+    document.querySelector(".img-apagada").style.display="none"
+    document.querySelector(".img-acesa").style.display="block"
+})
+
+const btnDesligar = document.querySelector(".btn_desligar")
+btnDesligar.addEventListener("click", ()=>{
+    document.querySelector(".img-apagada").style.display="block"
+    document.querySelector(".img-acesa").style.display="none"
+})
